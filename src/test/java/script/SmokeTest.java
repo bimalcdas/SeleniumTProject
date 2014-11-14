6ec60aa1186934fdc11141bfd63de4a932d81a1f
kamolini <kamolinibachar@gmail.com>
2014-11-13 22:38:37 -0500

    Commit SeleniumTProject

---
+++ src/test/java/script/SmokeTest.java
@@ -0,0 +1,17 @@
+package script;
+
+import org.testng.annotations.Test;
+
+public class SmokeTest {
+	
+	@Test
+	public void TitleValidationTest(){
+		
+		System.out.println("========================================================");
+		System.out.println("Kamolesh");
+		System.out.println("========================================================");
+
+		
+	}
+
+}
